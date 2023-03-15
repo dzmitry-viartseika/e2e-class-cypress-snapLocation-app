762f7ae290b630d56ce690d60e09d4721622ed43
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-03-15 21:16:55 +0100

    feat: implement e2e tests

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,24 +1,25 @@
 /// <reference types="cypress" />
-
-const SET_TIME_OUT_TIME = 1000;
+const TIMER = 2000;
 
 describe('share location', () => {
 
   beforeEach(() => {
+    cy.clock();
+    cy.fixture('user-location.json').as('userLocation');
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition').callsFake((cb) => {
-        setTimeout(() => {
-          cb({
-            coords: {
-              latitude: 37.45,
-              longitude: 48.01,
-            },
-          });
-        }, SET_TIME_OUT_TIME)
+      cy.get('@userLocation').then(fakePostion => {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition').callsFake((cb) => {
+          setTimeout(() => {
+            cb(fakePostion);
+          }, TIMER)
+        })
       })
       cy.get('[data-cy="get-loc-btn"]').as('getLocButton');
       cy.get('[data-cy="share-loc-btn"]').as('shareLocButton');
       cy.get('[data-cy="actions"]').as('actionsDiv');
+      cy.stub(win.navigator.clipboard, 'writeText').as('saveToClipboard').resolves()
+      cy.spy(win.localStorage, 'setItem').as('storeLocation');
+      cy.spy(win.localStorage, 'getItem').as('getStoredLocation');
     })
   })
 
@@ -34,7 +35,26 @@ describe('share location', () => {
   });
 
   it('should share a location link', () => {
+    const TOOLTIP_SUCCESS_TEXT = 'Location URL copied to clipboard.';
+    const TOOLTIP_STORED_TEXT = 'Stored location URL copied to clipboard.';
     cy.get('[data-cy="name-input"]').type('Dzmitry');
     cy.get('@getLocButton').click();
-  })
+    cy.get('@shareLocButton').click();
+    cy.get('@saveToClipboard').should('have.been.called');
+    cy.get('[data-cy="info-message"]').contains(TOOLTIP_SUCCESS_TEXT);
+    cy.get('@userLocation').then((fakePosition) => {
+      const { latitude, longitude } = fakePosition.coords;
+      cy.get('@saveToClipboard')
+          .should('have.been.calledWithMatch', new RegExp(`${latitude}.*${longitude}.*${encodeURI('Dzmitry')}`));
+      cy.get('@storeLocation').should('have.been.calledWithMatch', /Dzmitry/, new RegExp(`${latitude}.*${longitude}.*${encodeURI('Dzmitry')}`));
+    });
+    cy.get('@storeLocation').should('have.been.called');
+    cy.get('@shareLocButton').click();
+    cy.get('@getStoredLocation').should('have.been.called');
+    cy.get('[data-cy="info-message"]').should('be.visible');
+    cy.get('[data-cy="info-message"]').should('have.class', 'visible');
+    cy.get('[data-cy="info-message"]').contains(TOOLTIP_STORED_TEXT);
+    cy.tick(TIMER);
+    cy.get('[data-cy="info-message"]').should('not.be.visible');
+  });
 });
